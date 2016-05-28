2c6063fac21f8e3679dffae747005768593afd88
kadhiresan <kkadhir10@gmail.com>
2016-05-28 16:27:41 +0530

    my changes

--- login.php
+++ login.php
@@ -8,6 +8,9 @@
 	<link rel="stylesheet" href="css/chat.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<!-- <script src="https://apis.google.com/js/platform.js" async defer></script> -->
+	<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+	<meta name="google-signin-client_id" content="500096944978-99393b2kmjehngsir5ocbdpai7h4bmaa.apps.googleusercontent.com">
 </head>
 <body class="login-page">
 <?php include 'dbconnection.php';?>
@@ -16,19 +19,16 @@
 		<div class="wrapper">
 			<form action="" method="post" name="Login_Form" class="form-signin">       
 			    <h3 class="form-signin-heading">Sign In</h3>
-				 		 <hr class="colorgraph"><br>
+				<hr class="colorgraph"><br>
 				  
-				  <input type="text" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
-				  <input type="password" class="form-control" name="Password" placeholder="Password" required=""/>     		  
+				<input type="text" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
+				<input type="password" class="form-control" name="Password" placeholder="Password" required=""/>     		  
 				 
-				  <button class="btn btn-lg btn-primary btn-block btn-blocksss"  name="Submit" value="Login" type="Submit">Login</button>  
+				<button class="btn btn-lg btn-primary btn-block btn-blocksss"  name="Submit" value="Login" type="Submit">Login</button>  
 
-				  <div class="social-auth-links text-center" style="margin-bottom: 15px;">
-    
-     			 <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google</a>
-   				 </div>	
-				<a href="register.html" class="text-center">Register a new membership</a>
+   				<!-- <div class="g-signin2" data-width="343" data-onsuccess="onSignIn">Sign in using Google</div>	 -->
+   				<div id="my-signin2" data-with="100%" data-background-color="#dd4b39"></div>
+				<a href="signup.html" class="text-center">Register a new membership</a>
 			</form>		
 		</div>
 	</div>
@@ -37,4 +37,85 @@
 
 </style>
 </body>
+<script type="text/javascript">
+	// function onSignIn(googleUser) {
+	// 	var profile = googleUser.getBasicProfile();
+	// 	var data = {
+	// 		"google_id" : profile.getId(),
+	// 		"name" : profile.getName(),
+	// 		"user_img" : profile.getImageUrl(),
+	// 		"email" : profile.getEmail(),
+	// 		"gmail_login" : true,
+	// 		"password" : '',
+	// 		"repassword" : ''
+	// 	}
+
+	// 	console.log('user data: ' + JSON.stringify(data));
+	// 	$.ajax({
+	//         url: '/chat-zone/signup.php',
+	//         type: 'POST',
+	//         // dataType: "json",
+	//         data: data,
+	//         success: function(data, status){
+	//         	console.log('ajax success=', data);
+	//         	console.log('ajax status=', status);
+	//         },
+	//         error: function(xhr, desc, err) {
+	//           console.log('ajax error', xhr);
+	      
+	//         }
+ //      	});
+	// }
+
+	function onSuccess(googleUser) {
+      // console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+      	var profile = googleUser.getBasicProfile();
+		var data = {
+			"google_id" : profile.getId(),
+			"name" : profile.getName(),
+			"user_img" : profile.getImageUrl(),
+			"email" : profile.getEmail(),
+			"gmail_login" : true,
+			"password" : '',
+			"repassword" : ''
+		}
+
+		console.log('user data: ' + JSON.stringify(data));
+		$.ajax({
+	        url: '/chat-zone/signup.php',
+	        type: 'POST',
+	        // dataType: "json",
+	        data: data,
+	        success: function(data, status){
+	        	console.log('ajax success=', data);
+	        	console.log('ajax status=', status);
+	        },
+	        error: function(xhr, desc, err) {
+	          console.log('ajax error', xhr);
+	      
+	        }
+      	});
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+
+	function signOut() {
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function () {
+			console.log('User signed out.');
+		});
+  	}
+</script>
 </html>
--- signup.php
+++ signup.php
@@ -1,3 +1,43 @@
+<?php include 'dbconnection.php';
+	if($_REQUEST && !empty($_REQUEST['email'] && !empty($_REQUEST['name']))) {
+		$name = $_REQUEST['name'];
+		$email = $_REQUEST['email'];
+		$password = $_REQUEST['password'];
+		$repassword = $_REQUEST['repassword'];
+		$user_img = $_REQUEST['user_img'];
+		$gmail_login = $_REQUEST['gmail_login'];
+		// echo "name"; print_r($name);
+		$run = false;
+		if($gmail_login){
+			$sql = "INSERT INTO users (fullname, password, email, user_img) VALUES ('".$name."', '".$password."', '".$email."', '".$user_img."')";
+			$run = true;
+		}else{
+			if($password == $repassword){
+				$sql = "INSERT INTO users (fullname, password, email) VALUES ('".$name."', '".$password."', '".$email."')";
+				$run = true;
+			}
+		}
+
+		$receiver_data  = array(
+			'name' => $name,
+			'email' => $email,
+			'password' => $password,
+			'repassword' => $repassword,
+			'user_img' => $user_img,
+			'gmail_login' => $gmail_login
+		);
+		$output = array('status'=>'failed', 'message' => 'Something went wrong','receiver_data' => $receiver_data);
+
+		if($run){
+			if ($conn->query($sql) === TRUE) {
+			   // header( 'Location: index.php' );
+				$output = array('status'=>'success', 'message' => 'successfully user created','receiver_data' => $receiver_data);
+			}
+		}
+	    echo json_encode($output, false);
+	    exit();
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,24 +50,6 @@
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body class="login-page">
-<?php include 'dbconnection.php';
-
-if($_REQUEST) {
-	$name = $_REQUEST['name'];
-	$email = $_REQUEST['email'];
-	$password = $_REQUEST['password'];
-	$repassword = $_REQUEST['repassword'];
-
-	if($password == $repassword){
-		$sql = "INSERT INTO user (fullname, password, email)
-			VALUES ('".$name."', '".$password."', '".$email."')";
-
-		if ($conn->query($sql) === TRUE) {
-			header( 'Location: index.php' );
-		}
-	}
-}
-?>
 	<div class="container">
 		<div class="wrapper">
 			<form action="" method="post" name="Login_Form" class="form-signin">       
@@ -36,6 +58,8 @@ if($_REQUEST) {
 
 					<input type="text" class="form-control" name="name" placeholder="Full Name" required="" autofocus="" /> 
 					<input type="text" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
+					<input type="text" class="form-control" name="user_img" style="display: none" />
+					<input type="text" class="form-control" name="gmail_login" style="display: none" />
 					<input type="password" class="form-control" name="password" placeholder="Password" required=""/>  
 					<input type="password" class="form-control" name="repassword" placeholder="Retype password" required=""/>     		  
 					<button class="btn btn-lg btn-primary btn-block btn-blocksss"  name="Submit" value="Login" type="Submit">SignUp</button>  
@@ -49,9 +73,5 @@ if($_REQUEST) {
 
 		</div>
 	</div>
-
-<style type="text/css">
-
-</style>
 </body>
 </html>
