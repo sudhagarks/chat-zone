c973918234e6ec417a863f0686a690386008902a
sudhagar ks <sudhagar@mantralabsglobal.com>
2016-05-29 20:44:27 +0530

    changes

--- js/chat.js
+++ js/chat.js
@@ -83,7 +83,7 @@ $(document).ready(function(){
         console.log(msg);
         if(msg.chat_type == "new"){
             var current_page_url = $(location).attr('origin')+$(location).attr('pathname');
-            //window.location.href = current_page_url+"?t="+$.now()+"&cid="+msg.chat_id;
+            window.location.href = current_page_url+"?t="+$.now()+"&cid="+msg.chat_id;
         } else {
             var room_token = msg.room_token;
             var chat_html = msg.chat_html;
